cf6932e338af9aee543a78d64fe1cc2f62b516e5
Ma Lin <malincns@163.com>
2023-07-11 20:39:27 +0800

    add config file for readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+project = 'pyzstd module'
+author = 'Ma Lin'
+copyright = '2020-present, Ma Lin'
+language = 'en'
+
+master_doc = 'index'
+pygments_style = 'sphinx'
+extensions = ['sphinx_rtd_theme']
+html_theme = 'sphinx_rtd_theme'
